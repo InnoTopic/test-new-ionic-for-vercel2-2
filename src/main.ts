c21325b4efb2ace47cb3c9dd6ebbce6cfa437d55
Karol Depka <karol-depka@users.noreply.github.com>
2023-02-01 07:54:20 +0100

    add colored console.log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+console.log('%capp main.ts %cstarting', 'color: red; background-color: black', 'color: lightblue; background-color: darkblue')
+
 if (environment.production) {
   enableProdMode();
 }
